c94ba774a375ea9a43686b17a735ccd1233adb60
lifeofladi <dev@lifeofladi.com>
2022-05-11 13:16:19 -0700

    version structure setup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 const express = require("express");
 
+//Router for V1
+const v1Router = require("./v1/routes/v1-routes");
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// For testing purposes
-app.get("/", (req, res) => {
-  res.send("<h2>It's Working!</h2>");
-});
+// // For testing purposes
+// app.get("/", (req, res) => {
+//   res.send("<h2>It's Working!</h2>");
+// });
+
+app.use("/api/v1", v1Router);
 
 app.listen(PORT, () => {
   console.log(`API is listening on port ${PORT}`);
