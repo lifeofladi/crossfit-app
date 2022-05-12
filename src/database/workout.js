c7c2663195cae6638967ed3402cf19d8f5dff4f8
lifeofladi <dev@lifeofladi.com>
2022-05-12 02:51:21 -0700

    implemented logic for creating workout - POST

--- src/database/workout.js
+++ src/database/workout.js
@@ -1,7 +1,19 @@
 const DB = require("./db.json");
+const { saveToDatabase } = require("./utils");
 
 const getAllWorkouts = () => {
   return DB.workouts;
 };
 
-module.exports = { getAllWorkouts };
+const createNewWorkout = newWorkout => {
+  //check to see if workout exists
+  const isAlreadyAdded =
+    DB.workouts.findIndex(workout => workout.name === newWorkout.name) > -1;
+  if (isAlreadyAdded) return;
+
+  DB.workouts.push(newWorkout);
+  saveToDatabase(DB);
+  return newWorkout;
+};
+
+module.exports = { getAllWorkouts, createNewWorkout };
--- src/services/workoutService.js
+++ src/services/workoutService.js
@@ -12,10 +12,10 @@ const getOneWorkout = () => {
 
 const createNewWorkout = newWorkout => {
   const workoutToInsert = {
-    ...newWorkout,
     id: uuid(),
     createdAt: new Date().toLocaleString("en-US", { timeZone: "UTC" }),
     updatedAt: new Date().toLocaleString("en-US", { timeZone: "UTC" }),
+    ...newWorkout,
   };
   return Workout.createNewWorkout(workoutToInsert);
 };
